e3a187eac8623be3ce8a639d1cccb0c5d78e1b7c
Mark Litwintschik <mark@marksblogg.com>
2025-11-16 20:21:58 +0200

    Use the latest Overture release (#143)

--- gpq_downloader/dialog.py
+++ gpq_downloader/dialog.py
@@ -1,5 +1,7 @@
 import json
 
+import requests
+
 from qgis.PyQt.QtWidgets import (
     QMessageBox,
     QDialog,
@@ -353,6 +355,8 @@ class DataSourceDialog(QDialog):
         if self.custom_radio.isChecked():
             return [self.url_input.text().strip()]
         elif self.overture_radio.isChecked():
+            latest_release = requests.get('https://labs.overturemaps.org/data/releases.json').json()['latest']
+
             for theme, checkbox in self.overture_checkboxes.items():
                 if checkbox.isChecked():
                     dataset = self.PRESET_DATASETS['overture'][theme]
@@ -366,11 +370,11 @@ class DataSourceDialog(QDialog):
                         # Handle multiple base subtypes
                         for subtype, subtype_checkbox in self.base_subtype_checkboxes.items():
                             if subtype_checkbox.isChecked():
-                                urls.append(dataset['url_template'].format(subtype=subtype))
+                                urls.append(dataset['url_template'].format(subtype=subtype, release=latest_release))
                         continue  # Skip the normal URL append for base
                     else:
                         type_str = theme.rstrip('s')  # remove trailing 's' for singular form
-                    urls.append(dataset['url_template'].format(subtype=type_str))
+                    urls.append(dataset['url_template'].format(subtype=type_str, release=latest_release))
         elif self.sourcecoop_radio.isChecked():
             selection = self.sourcecoop_combo.currentText()
             dataset = next((dataset for dataset in self.PRESET_DATASETS['source_cooperative'].values()
